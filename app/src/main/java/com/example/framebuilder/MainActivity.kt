9edab79272b26b88fc9b3fd053f3aedfdec595f0
Farukh <joombey@yandex.ru>
2022-10-28 16:31:57 +0300

    initial state

---
+++ app/src/main/java/com/example/framebuilder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.framebuilder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
